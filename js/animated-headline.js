4448bdc21b00b8efb6f6b5bc0e3f62befd66e542
pietelite <pieter2@illinois.edu>
2019-07-15 21:39:19 -0400

    testing headline speed

--- js/animated-headline.js
+++ js/animated-headline.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($){
 	//set animation timing
-	var animationDelay = 3000,
+	var animationDelay = 500,
 		//loading bar effect
 		barAnimationDelay = 3800,
 		barWaiting = barAnimationDelay - 3000, //3000 is the duration of the transition on the loading bar - set in the scss/css file
